e6382ac5e27ff128d581ae2481e5ca8c99f89b3c
azmithajeni <155456070+azmithajeni@users.noreply.github.com>
2024-02-21 17:30:41 +0530

    Create task7.py

---
+++ task7.py
@@ -0,0 +1,13 @@
+a = int(input())
+b = int(input())
+operation =input("add/sub/mul/div:")
+if(operation=="add"):
+    print("add :",a+b)
+elif(operation=="sub"):
+    print("sub :",a-b)
+elif(operation=="mul"):
+    print("mul :",a*b)
+elif(operation=="div"):
+    print("div :",a/b)
+else:
+    print("Invalid")
