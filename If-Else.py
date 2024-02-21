cf94631fd7fee50f2cecb66b6f5081673213ad38
azmithajeni <155456070+azmithajeni@users.noreply.github.com>
2024-02-21 11:10:20 +0530

    Create If-Else Task1.py

---
+++ If-Else.py
@@ -0,0 +1,6 @@
+Qn : Get user input for variable meghna, if meghna == died print "Surya meets Priya" else "Surya weds Meghna"
+megana = input()
+if(megana =="died"):
+    print("surya meets priya")
+else:
+    print("surya weds meghna")
