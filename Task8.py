02fc5f8026a7ded40913830d4ee6ad8df837b28a
azmithajeni <155456070+azmithajeni@users.noreply.github.com>
2024-02-21 17:48:24 +0530

    Create Task8.py

---
+++ Task8.py
@@ -0,0 +1,8 @@
+score = int(input("Score Percentage :"))
+if(score > 70):
+    name=input("Name :")
+    dept = input("Dept :")
+    location=input("location :")
+    print("You are eligible")
+else:
+    print("Not eligible")
